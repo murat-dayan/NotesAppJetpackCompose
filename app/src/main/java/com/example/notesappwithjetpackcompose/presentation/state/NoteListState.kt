82dc3c7e83e59fd3392d1f007db3504313ae2b37
muratdayan <muratdayan359@gmail.com>
2024-04-04 21:55:06 +0300

    mvvm arch added

--- app/src/main/java/com/example/notesappwithjetpackcompose/presentation/state/NoteListState.kt
+++ app/src/main/java/com/example/notesappwithjetpackcompose/presentation/state/NoteListState.kt
@@ -2,8 +2,8 @@ package com.example.notesappwithjetpackcompose.presentation.state
 
 import com.example.notesappwithjetpackcompose.domain.model.Note
 
-data class NoteState(
-    val note: Note? = null,
+data class NoteListState(
+    val notes: List<Note>? = emptyList(),
     val errorMsg: String ?= "",
     val isLoading : Boolean = false
 )
